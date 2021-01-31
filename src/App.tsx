cbe9e3816319e5d670e747a1893ba2545199bf63
StanislavKaisin <8050361@gmail.com>
2021-01-31 19:45:34 +0200

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Library } from "./pages/Library";
+import { Movie } from "./pages/Movie";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/library">
+          <Library />
+        </Route>
+        <Route exact path="/movie">
+          <Movie />
+        </Route>
+      </Switch>
+    </>
+  );
 }
 
 export default App;
