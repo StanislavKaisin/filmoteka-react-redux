c6fd43692ea9b35ddbc0dba89ad6640dc063a201
StanislavKaisin <8050361@gmail.com>
2021-02-08 21:25:37 +0200

    removed interfaces

--- src/pages/Movie.tsx
+++ src/pages/Movie.tsx
@@ -12,39 +12,6 @@ import React from "react";
 
 import mockMovie from "../mockData/mockMovie.json";
 
-interface IRating {
-  Source: string;
-  Value: string;
-}
-
-export interface IMovie {
-  Title: string;
-  Year: string;
-  Rated: string;
-  Released: string;
-  Runtime: string;
-  Genre: string;
-  Director: string;
-  Writer: string;
-  Actors: string;
-  Plot: string;
-  Language: string;
-  Country: string;
-  Awards: string;
-  Poster: string;
-  Ratings: IRating[];
-  Metascore: string;
-  imdbRating: string;
-  imdbVotes: string;
-  imdbID: string;
-  Type: string;
-  DVD: string;
-  BoxOffice: string;
-  Production: string;
-  Website: string;
-  Response: string;
-}
-
 const useStyles = makeStyles({
   title: {
     fontSize: "2rem",
@@ -67,8 +34,6 @@ export const Movie = () => {
   const styles = useStyles();
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.up("sm"));
-  // console.log("mockMovie", mockMovie);
-  console.log("matches", matches);
 
   const {
     Poster,
