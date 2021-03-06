19ba155246da94bc6fdd53baede7418adec1be90
StanislavKaisin <8050361@gmail.com>
2021-03-06 20:03:51 +0200

    added library page

--- src/components/PaginationList.tsx
+++ src/components/PaginationList.tsx
@@ -10,6 +10,8 @@ import {
   Theme,
   Toolbar,
   Typography,
+  useMediaQuery,
+  useTheme,
 } from "@material-ui/core";
 import Pagination from "@material-ui/lab/Pagination";
 import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
@@ -22,6 +24,10 @@ import { ScrollTop } from "./ScrollTop";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+    positionContainer: { position: "absolute" },
+    positionRelative: {
+      position: "relative",
+    },
     container: {
       padding: "1rem 0 1rem 0",
     },
@@ -29,7 +35,7 @@ const useStyles = makeStyles((theme: Theme) =>
       margin: "1rem",
     },
     media: {
-      [theme.breakpoints.down("xs")]: {
+      [theme.breakpoints.down("md")]: {
         height: 270,
         width: 180,
       },
@@ -38,77 +44,109 @@ const useStyles = makeStyles((theme: Theme) =>
       marginLeft: "auto",
       marginRight: "auto",
     },
+    message: {
+      margin: "1rem",
+      color: "rgba(0, 0, 0, 0.54)",
+      textTransform: "uppercase",
+      // fontWeight: "bolder",
+    },
   })
 );
 
-interface IPaginationList {
+export interface IPaginationList {
   moviesList: ICard[];
 }
 
 export const PaginationList = ({ moviesList }: IPaginationList) => {
+  console.log("PaginationList=");
   const classes = useStyles();
   const pagesCount = getPagesCount(moviesList.length);
   const splittedMoviesList = splitMoviesList(moviesList, pagesCount);
-  console.log("moviesList", moviesList);
-  console.log("splittedMoviesList=", splittedMoviesList);
+  // console.log("moviesList", moviesList);
+
   const [displayedMoviesList, setdisplayedMoviesList] = useState(
     splittedMoviesList[0]
   );
+  // console.log("displayedMoviesList=", displayedMoviesList);
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("sm"));
+  console.log("theme=", theme);
+  console.log("matches=", matches);
+
   const handleChange = (event: React.ChangeEvent<any>, page: number) => {
     setdisplayedMoviesList(splittedMoviesList[page - 1]);
   };
 
   return (
     <>
-      <Toolbar id="back-to-top-anchor" />
-      <Grid container className={classes.container} spacing={2}>
-        <Grid container justify="center" direction="row" wrap="wrap">
-          {displayedMoviesList.map((movie: ICard) => (
-            <Grid item key={movie.imdbID} xs={12} sm={6} md={4}>
-              <Card className={classes.root}>
-                <CardActionArea>
-                  <CardMedia
-                    image={movie.Poster}
-                    title={movie.Title}
-                    className={classes.media}
-                  />
-                  <CardContent>
-                    <Typography
-                      gutterBottom
-                      variant="h5"
-                      component="h2"
-                      align="center"
-                    >
-                      {movie.Title}
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      color="textSecondary"
-                      component="p"
-                    >
-                      Year: {movie.Year}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
+      <Grid
+        container
+        // className={classes.positionContainer}
+      >
+        <Toolbar id="back-to-top-anchor" />
+        <Grid container className={classes.container} spacing={2}>
+          <Grid
+            container
+            justify="center"
+            direction="row"
+            wrap="wrap"
+            // className={classes.positionContainer}
+          >
+            {displayedMoviesList ? (
+              displayedMoviesList.map((movie: ICard) => (
+                <Grid item key={movie.imdbID} xs={12} sm={6} md={4}>
+                  <Card className={classes.root}>
+                    <CardActionArea>
+                      <CardMedia
+                        image={movie.Poster}
+                        title={movie.Title}
+                        className={classes.media}
+                      />
+                      <CardContent>
+                        <Typography
+                          gutterBottom
+                          variant="h5"
+                          component="h2"
+                          align="center"
+                        >
+                          {movie.Title}
+                        </Typography>
+                        <Typography
+                          variant="body2"
+                          color="textSecondary"
+                          component="p"
+                        >
+                          Year: {movie.Year}
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </Grid>
+              ))
+            ) : (
+              <Typography align="center" className={classes.message}>
+                Nothing here
+              </Typography>
+            )}
+          </Grid>
+          {splittedMoviesList.length ? (
+            <Grid container justify="center" direction="row">
+              <Pagination
+                count={pagesCount}
+                size="large"
+                onChange={(event: React.ChangeEvent<any>, page: number) =>
+                  handleChange(event, page)
+                }
+              />
             </Grid>
-          ))}
+          ) : null}
         </Grid>
-        <Grid container justify="center" direction="row">
-          <Pagination
-            count={pagesCount}
-            size="large"
-            onChange={(event: React.ChangeEvent<any>, page: number) =>
-              handleChange(event, page)
-            }
-          />
-        </Grid>
+        <ScrollTop {...null} className="positionRelative">
+          <Fab size="medium" aria-label="scroll back to top">
+            <KeyboardArrowUpIcon />
+          </Fab>
+        </ScrollTop>
       </Grid>
-      <ScrollTop {...null}>
-        <Fab size="medium" aria-label="scroll back to top">
-          <KeyboardArrowUpIcon />
-        </Fab>
-      </ScrollTop>
     </>
   );
 };
--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -1,5 +1,133 @@
-import React from "react";
+import React, { useState } from "react";
+// import Paper from "@material-ui/core/Paper";
+import { makeStyles, Theme, useTheme } from "@material-ui/core/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import VisibilityIcon from "@material-ui/icons/Visibility";
+import { AppBar, Box, Grid, Typography } from "@material-ui/core";
+// import SwipeableViews from "react-swipeable-views";
+
+import mockList from "../mockData/mockList.json";
+// import { ICard } from "../interfaces/ICard";
+import { PaginationList } from "../components/PaginationList";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: { width: "80%" },
+    [theme.breakpoints.up("md")]: { width: "65%" },
+  },
+  library: {
+    paddingTop: "1rem",
+  },
+}));
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  dir?: string;
+  index: any;
+  value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: any) {
+  return {
+    id: `full-width-tab-${index}`,
+    "aria-controls": `full-width-tabpanel-${index}`,
+  };
+}
 
 export const Library = () => {
-  return <>Library page</>;
+  // console.log("mockList.Search", mockList.Search);
+  const classes = useStyles();
+  const theme = useTheme();
+
+  const [value, setValue] = React.useState(0);
+  const lists = [[], [], mockList.Search];
+
+  console.log("value", value);
+  console.log("lists[value]=", lists[value]);
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <>
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        className={classes.library}
+      >
+        <div className={classes.root}>
+          {/* <Paper square> */}
+          <AppBar position="static" color="transparent" elevation={0}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              indicatorColor="secondary"
+              textColor="secondary"
+              variant="fullWidth"
+              aria-label="full width tabs example"
+            >
+              <Tab
+                label="PLANNED"
+                icon={<WatchLaterIcon />}
+                {...a11yProps(0)}
+              />
+              <Tab
+                label="WATCHED"
+                icon={<VisibilityIcon />}
+                {...a11yProps(1)}
+              />
+              <Tab
+                label="FAVORITES"
+                icon={<FavoriteIcon />}
+                {...a11yProps(2)}
+              />
+            </Tabs>
+          </AppBar>
+          {/* <SwipeableViews
+            axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+            index={value}
+            onChangeIndex={handleChangeIndex}
+          > */}
+          <TabPanel value={value} index={0} dir={theme.direction}>
+            <PaginationList moviesList={lists[0]} />
+          </TabPanel>
+          <TabPanel value={value} index={1} dir={theme.direction}>
+            <PaginationList moviesList={lists[1]} />
+          </TabPanel>
+          <TabPanel value={value} index={2} dir={theme.direction}>
+            <PaginationList moviesList={lists[2]} />
+          </TabPanel>
+          {/* </SwipeableViews> */}
+          {/* </Paper> */}
+        </div>
+      </Grid>
+    </>
+  );
 };
