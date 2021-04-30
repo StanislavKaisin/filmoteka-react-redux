6bf23e577af93019b424a46e727c373c63d779e7
StanislavKaisin <8050361@gmail.com>
2021-04-30 21:06:42 +0300

    fixed movie page styles

--- src/pages/Movie.tsx
+++ src/pages/Movie.tsx
@@ -1,8 +1,10 @@
 import {
   Button,
+  createStyles,
   Grid,
   IconButton,
   makeStyles,
+  Theme,
   Typography,
   useMediaQuery,
   useTheme,
@@ -16,44 +18,57 @@ import { IState } from "../redux/rootReducer";
 
 // import mockMovie from "../mockData/mockMovie.json";
 
-const useStyles = makeStyles({
-  container: {
-    marginTop: "1rem",
-  },
-  title: {
-    fontSize: "2rem",
-    fontWeight: "bold",
-  },
-  year: {
-    color: "#9e9e9e",
-  },
-  infoTitle: {
-    fontSize: "1.1rem",
-    fontWeight: "bold",
-  },
-  votes: {
-    color: "#9e9e9e",
-    fontSize: "0.9rem",
-  },
-});
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      marginTop: "1rem",
+      [theme.breakpoints.up("sm")]: {
+        width: "90%",
+        margin: "0 auto",
+      },
+      [theme.breakpoints.up("md")]: { width: "80%" },
+      [theme.breakpoints.up("lg")]: { width: "75%" },
+    },
+    descriptionContainer: {
+      width: "80%",
+    },
+    title: {
+      fontSize: "2rem",
+      fontWeight: "bold",
+    },
+    year: {
+      color: "#9e9e9e",
+    },
+    infoTitle: {
+      fontSize: "1.1rem",
+      fontWeight: "bold",
+    },
+    votes: {
+      color: "#9e9e9e",
+      fontSize: "0.9rem",
+    },
+  })
+);
 
 export const Movie = () => {
-  const styles = useStyles();
   const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.up("sm"));
+  const styles = useStyles();
+  const matchesSm = useMediaQuery(theme.breakpoints.up("sm"));
+
   const history = useHistory();
   const dispatch = useDispatch();
-  console.log(`history`, history);
+  // console.log(`history`, history);
   // const [movie, setmovie] = useState(null);
   useEffect(() => {
-    console.log(`history`, history);
+    // console.log(`history`, history);
     const movieId = history.location.search.split("=")[1];
     dispatch(fetchMovie(movieId));
   }, []);
   const movie = useSelector((state: IState) => state.movie?.movie!);
-  console.log(`movie`, movie);
+  // console.log(`movie`, movie);
   // setmovie(useSelector((state: IState) => state.movie));
   // const movie = undefined;
+
   if (!movie) return null;
   const {
     Poster,
@@ -74,16 +89,16 @@ export const Movie = () => {
       {movie ? (
         <Grid
           container
-          direction={matches ? "row" : "column"}
-          alignItems={matches ? "flex-start" : "center"}
-          wrap={matches ? "nowrap" : "wrap"}
+          direction={matchesSm ? "row" : "column"}
+          alignItems={matchesSm ? "flex-start" : "center"}
+          wrap={matchesSm ? "nowrap" : "wrap"}
           spacing={2}
           className={styles.container}
         >
           <Grid item>
             <img src={Poster} alt={Title} />
           </Grid>
-          <Grid item>
+          <Grid item className={styles.descriptionContainer}>
             <Typography variant="h1" className={styles.title}>
               {Title}
               <Typography
